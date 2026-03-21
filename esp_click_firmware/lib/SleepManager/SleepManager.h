ceb213d405fa1545b5473a8b902a177b3e940d01
madhukartemba <madhusmiles.madhukar@gmail.com>
2026-03-22 03:02:29 +0530

    add sleep manager

--- esp_click_firmware/lib/SleepManager/SleepManager.h
+++ esp_click_firmware/lib/SleepManager/SleepManager.h
@@ -6,11 +6,45 @@ class SleepManager
 private:
     EventGroupHandle_t sleepEventGroup;
 
+    EventBits_t registeredTasksMask = 0;
+
     int eventCount = 0;
+    unsigned long sleepTimeout = 10000;
 
     unsigned long lastActivityTime = 0;
 
     SleepManager() {}
+
+    static void sleepTask(void *pvParameters)
+    {
+        SleepManager *instance = (SleepManager *)pvParameters;
+        instance->run();
+    }
+
+    void run()
+    {
+        while (true)
+        {
+            if (eventCount > 0)
+            {
+                xEventGroupWaitBits(sleepEventGroup, registeredTasksMask, pdFALSE, pdTRUE, portMAX_DELAY);
+            }
+
+            unsigned long idleTime = millis() - lastActivityTime;
+
+            if (idleTime >= sleepTimeout)
+            {
+                // Simulate sleep
+                Serial.println("Going into deep sleep");
+                vTaskDelay(pdMS_TO_TICKS(1000000));
+            }
+            else
+            {
+                unsigned long waitTime = sleepTimeout - idleTime;
+                vTaskDelay(pdMS_TO_TICKS(waitTime));
+            }
+        }
+    }
 
 public:
     static SleepManager &getInstance()
@@ -30,6 +64,8 @@ public:
         {
             Serial.println("Failed to create sleep event group!");
         }
+
+        xTaskCreate(SleepManager::sleepTask, "Sleep Manager", 2048, this, 1, NULL);
     }
 
     EventBits_t registerTask()
@@ -44,24 +80,31 @@ public:
 
         eventCount++;
 
-        Serial.println("Task registered with sleep manager, assigned bit: " + String(assignedBit, BIN) + " (Event count: " + String(eventCount) + ")");
+        registeredTasksMask |= assignedBit;
+
+        xEventGroupSetBits(sleepEventGroup, assignedBit);
 
         return assignedBit;
     }
 
     void keepAwake(EventBits_t taskId)
     {
-        xEventGroupSetBits(sleepEventGroup, taskId);
+        xEventGroupClearBits(sleepEventGroup, taskId);
     }
 
     void allowSleep(EventBits_t taskId)
     {
-        xEventGroupClearBits(sleepEventGroup, taskId);
+        xEventGroupSetBits(sleepEventGroup, taskId);
         lastActivityTime = millis();
     }
 
     void reportActivity()
     {
         lastActivityTime = millis();
+    }
+
+    void setSleepTimeout(unsigned long timeoutMs)
+    {
+        sleepTimeout = timeoutMs;
     }
 };
